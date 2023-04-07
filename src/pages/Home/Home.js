496778408e337906065c2c6a2a819f33753ef7d0
NayaOnwubiko <naya.onwubiko@gmail.com>
2023-04-07 17:11:34 -0400

    home page mobile view styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import "./Home.scss";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import hero from "../../assets/logo/hero.svg";
 
 function Home() {
     const navigate = useNavigate();
@@ -46,13 +46,23 @@ function Home() {
     //Show the user the login form
     return (
         <>
-            <h1>Please login</h1>
-            <form onSubmit={handleLogin}>
-                <label>Email: <input type="text" name="email"></input></label><br />
-                <label>Password: <input type="password" name="password"></input></label><br />
-                <button>Login</button>
-                <Link to="/signup">Sign Up</Link>
-            </form>
+            <section className="home">
+                <div className="home__hero">
+                    <h1 className="home__hero-title">HEALTHY AND BEAUTIFUL BODY</h1>
+                    <img className="home__hero-image" 
+                        src={hero}
+                        alt="hero" />
+                </div>
+                <form className="home__form" onSubmit={handleLogin}>
+                    <label className="home__form-label">Email
+                        <input className="home__form-input" type="text" name="email"></input></label>
+                    <label className="home__form-label">Password 
+                        <input className="home__form-input" type="password" name="password"></input></label>
+                    <Link to="/signup" className="home__form-link">Create an account</Link>
+                    <button className="home__form-button">SIGN IN</button>
+                    
+                </form>
+            </section>
         </>
     )
 
